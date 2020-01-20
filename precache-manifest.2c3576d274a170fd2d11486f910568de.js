38626de5d1cbdc297df61948c37aa077ea493b3e
Eric Arnez <ericarnez@gmail.com>
2020-01-20 09:41:07 -0300

    Updates

---
+++ precache-manifest.2c3576d274a170fd2d11486f910568de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b08c5a00e766d6d60ae54400624324",
+    "url": "/trinomioChallenge/index.html"
+  },
+  {
+    "revision": "13097bf6cfec753e3cfa",
+    "url": "/trinomioChallenge/static/css/2.8ba03ab4.chunk.css"
+  },
+  {
+    "revision": "d567e34ba298f879014d",
+    "url": "/trinomioChallenge/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "13097bf6cfec753e3cfa",
+    "url": "/trinomioChallenge/static/js/2.d42e258a.chunk.js"
+  },
+  {
+    "revision": "8035939156decb7c38a027cff88318a2",
+    "url": "/trinomioChallenge/static/js/2.d42e258a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d567e34ba298f879014d",
+    "url": "/trinomioChallenge/static/js/main.ac5c435f.chunk.js"
+  },
+  {
+    "revision": "75f37f95d11b5a31dade",
+    "url": "/trinomioChallenge/static/js/runtime-main.58c354ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EricArnez/trinomioChallenge/precache-manifest.2e5dc01f968603aedcce0efc60444b51.js"
+  "/trinomioChallenge/precache-manifest.2c3576d274a170fd2d11486f910568de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EricArnez/trinomioChallenge/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trinomioChallenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
